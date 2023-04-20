f680c9f75af5e2df45e82c5f3e9d73f4bf8d40a9
Łukasz <131209055+lukaszs03@users.noreply.github.com>
2023-04-20 02:28:07 +0200

    Update ciagarytmetyczny.py
    func to func

--- ciagarytmetyczny.py
+++ ciagarytmetyczny.py
@@ -13,38 +13,30 @@ def sumuj_do(liczba):
 
     return (suma)
 
-start = time.perf_counter()
-print(sumuj_do(0))
-print(finish_timer(start))
-
 def sumuj_do2(wpis):
     wpis = int(input("Podaj ilość liczb: "))
     return (1 + wpis) / 2 * wpis
-
-start = time.perf_counter()
-print(sumuj_do2(0))
-print(finish_timer(start))
 
 def sumuj_do3(wpis):
     wpis = int(input("Podaj ilość liczb: ")) + 1
     return sum([wpis for wpis in range(1,wpis)])
 
-start = time.perf_counter()
-print(sumuj_do3(0))
-print(finish_timer(start))
-
 def sumuj_do4(wpis):
     wpis = int(input("Podaj ilość liczb: ")) + 1
     return sum({wpis for wpis in range(1,wpis)})
-
-start = time.perf_counter()
-print(sumuj_do4(0))
-print(finish_timer(start))
 
 def sumuj_do5(wpis):
     wpis = int(input("Podaj ilość liczb: ")) + 1
     return sum((wpis for wpis in range(1,wpis)))
 
-start = time.perf_counter()
-print(sumuj_do5(0))
-print(finish_timer(start))
+def function_performance(func, arg):
+    start = time.perf_counter()
+    func(arg)
+    end = time.perf_counter()
+    return end - start
+
+print(function_performance(sumuj_do, 0))
+print(function_performance(sumuj_do2, 0))
+print(function_performance(sumuj_do3, 0))
+print(function_performance(sumuj_do4, 0))
+print(function_performance(sumuj_do5, 0))
