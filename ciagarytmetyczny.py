8cb8248310287b141e645f581b49a1e9c321f6dd
Łukasz <131209055+lukaszs03@users.noreply.github.com>
2023-04-20 02:09:08 +0200

    Update ciagarytmetyczny.py
    use of the time import is not really good here I think when we deal with user input

--- ciagarytmetyczny.py
+++ ciagarytmetyczny.py
@@ -1,3 +1,9 @@
+import time
+
+def finish_timer(start):
+    end = time.perf_counter()
+    return end-start
+
 def sumuj_do(liczba):
 
     suma = 0
@@ -7,28 +13,38 @@ def sumuj_do(liczba):
 
     return (suma)
 
+start = time.perf_counter()
 print(sumuj_do(0))
+print(finish_timer(start))
 
 def sumuj_do2(wpis):
     wpis = int(input("Podaj ilość liczb: "))
     return (1 + wpis) / 2 * wpis
 
+start = time.perf_counter()
 print(sumuj_do2(0))
+print(finish_timer(start))
 
 def sumuj_do3(wpis):
     wpis = int(input("Podaj ilość liczb: ")) + 1
     return sum([wpis for wpis in range(1,wpis)])
 
+start = time.perf_counter()
 print(sumuj_do3(0))
+print(finish_timer(start))
 
 def sumuj_do4(wpis):
     wpis = int(input("Podaj ilość liczb: ")) + 1
     return sum({wpis for wpis in range(1,wpis)})
 
+start = time.perf_counter()
 print(sumuj_do4(0))
+print(finish_timer(start))
 
 def sumuj_do5(wpis):
     wpis = int(input("Podaj ilość liczb: ")) + 1
     return sum((wpis for wpis in range(1,wpis)))
 
+start = time.perf_counter()
 print(sumuj_do5(0))
+print(finish_timer(start))
