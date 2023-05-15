54a7c5b438b3f26969f14fc4c3ba979b2f7a4007
Łukasz <131209055+lukaszs03@users.noreply.github.com>
2023-05-15 20:08:54 +0200

    Update award.py

--- award.py
+++ award.py
@@ -2,6 +2,7 @@ import requests
 import json 
 
 r = requests.get("https://jsonplaceholder.typicode.com/todos")
+a = requests.get("https://jsonplaceholder.typicode.com/users")
 
 def count_task_frequency(tasks): 
     completedTasksFrequencyByUser = dict()
@@ -23,11 +24,19 @@ def users_with_top_completed_tasks(completedTasksFrequencyByUser):
 
     return usersIdWithMaxCompletedAmountOfTasks
 
+def naming_users(usersWithTopCompletedTasks):
+    for user in users: 
+        if(user["id"] in usersWithTopCompletedTasks):
+            print("The cookie award is going to users:", user["name"])
+    return usersWithTopCompletedTasks
+
 try:
     tasks = r.json()
+    users = a.json()
 except json.decoder.JSONDecodeError:
     print("Invalid format")
 else:
     completedTasksFrequencyByUser = count_task_frequency(tasks)
-    users_with_top_completed_tasks = users_with_top_completed_tasks(completedTasksFrequencyByUser)
-    print("The cookie award is going to users with ID:", users_with_top_completed_tasks)
+    usersWithTopCompletedTasks = users_with_top_completed_tasks(completedTasksFrequencyByUser)
+    naming_users(usersWithTopCompletedTasks)
+    #print("The cookie award is going to users with ID:", users_with_top_completed_tasks)
